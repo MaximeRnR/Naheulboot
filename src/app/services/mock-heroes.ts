9aed6b1349978b96c791aadd6df78e6bf2ae498c
mreynier@excilys <mreynier@excilys.com>
2017-05-29 13:34:38 +0200

    Specific avatar depending on hero job

--- src/app/services/mock-heroes.ts
+++ src/app/services/mock-heroes.ts
@@ -1,58 +1,313 @@
-import { Hero } from '../core/models/hero';
+import {Hero} from '../core/models/hero';
 
 export const HEROES: Hero[] = [
     {
-      id: 1,
-      name: "Ariok",
-      sexe: "Masculin",
-      job: "Ranger",
-      race: "Demi-Elfe",
-      level: 4,
-      experience: 405,
-      destiny_point: 3,
-      gold: 528,
+      id: 25,
+      name: 'Simmons',
+      sexe: 'Masculin',
+      job: 'Mage',
+      race: 'Humain',
+      level: 1,
+      experience: 258,
+      destiny_point: 0,
+      gold: 583,
       stats: {
-        bravery: 10,
-        intelligence: 11,
+        bravery: 13,
+        intelligence: 10,
         charisma: 11,
-        dexterity: 14,
-        strength: 8,
-        health: 38,
+        dexterity: 11,
+        strength: 11,
+        health: 25,
         mana: 0,
         armor: 0,
         attack: 10,
         parry: 10
       },
-      skills: [ {name: "Test", effect: "test"}, {name: "Test", effect: "test"} ],
+      skills: [
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        }
+      ],
       weapons: [],
       protections: [],
-      description: "Demi Elfe Ranger Miséricordieux"
-    }, {
-      id: 1,
-      name: "Ariok",
-      sexe: "Masculin",
-      job: "Ranger",
-      race: "Demi-Elfe",
-      level: 4,
-      experience: 405,
-      destiny_point: 3,
-      gold: 528,
+      description: 'description'
+    },
+    {
+      id: 28,
+      name: 'Sweet',
+      sexe: 'Masculin',
+      job: 'Guerrier',
+      race: 'Nain',
+      level: 5,
+      experience: 186,
+      destiny_point: 1,
+      gold: 777,
       stats: {
-        bravery: 10,
-        intelligence: 11,
-        charisma: 11,
-        dexterity: 14,
-        strength: 8,
-        health: 38,
+        bravery: 13,
+        intelligence: 10,
+        charisma: 12,
+        dexterity: 10,
+        strength: 10,
+        health: 25,
         mana: 0,
         armor: 0,
         attack: 10,
         parry: 10
       },
-      skills: [ {name: "Test", effect: "test"}, {name: "Test", effect: "test"} ],
+      skills: [
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        }
+      ],
       weapons: [],
       protections: [],
-      description: "Demi Elfe Ranger Miséricordieux"
+      description: 'description'
+    },
+    {
+      id: 27,
+      name: 'Alyson',
+      sexe: 'Masculin',
+      job: 'Guerrier',
+      race: 'Elfe',
+      level: 1,
+      experience: 168,
+      destiny_point: 2,
+      gold: 148,
+      stats: {
+        bravery: 8,
+        intelligence: 13,
+        charisma: 8,
+        dexterity: 10,
+        strength: 8,
+        health: 25,
+        mana: 0,
+        armor: 0,
+        attack: 10,
+        parry: 10
+      },
+      skills: [
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        }
+      ],
+      weapons: [],
+      protections: [],
+      description: 'description'
+    },
+    {
+      id: 39,
+      name: 'Lenora',
+      sexe: 'Feminin',
+      job: 'Mage',
+      race: 'Nain',
+      level: 5,
+      experience: 502,
+      destiny_point: 0,
+      gold: 989,
+      stats: {
+        bravery: 11,
+        intelligence: 9,
+        charisma: 8,
+        dexterity: 13,
+        strength: 13,
+        health: 25,
+        mana: 0,
+        armor: 0,
+        attack: 10,
+        parry: 10
+      },
+      skills: [
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        }
+      ],
+      weapons: [],
+      protections: [],
+      description: 'description'
+    },
+    {
+      id: 23,
+      name: 'Wendi',
+      sexe: 'Masculin',
+      job: 'Voleur',
+      race: 'Nain',
+      level: 5,
+      experience: 302,
+      destiny_point: 3,
+      gold: 204,
+      stats: {
+        bravery: 13,
+        intelligence: 12,
+        charisma: 13,
+        dexterity: 8,
+        strength: 9,
+        health: 25,
+        mana: 0,
+        armor: 0,
+        attack: 10,
+        parry: 10
+      },
+      skills: [
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        }
+      ],
+      weapons: [],
+      protections: [],
+      description: 'description'
+    },
+    {
+      id: 30,
+      name: 'Shelley',
+      sexe: 'Masculin',
+      job: 'Voleur',
+      race: 'Humain',
+      level: 2,
+      experience: 362,
+      destiny_point: 1,
+      gold: 408,
+      stats: {
+        bravery: 11,
+        intelligence: 11,
+        charisma: 8,
+        dexterity: 11,
+        strength: 8,
+        health: 25,
+        mana: 0,
+        armor: 0,
+        attack: 10,
+        parry: 10
+      },
+      skills: [
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        }
+      ],
+      weapons: [],
+      protections: [],
+      description: 'description'
+    },
+    {
+      id: 31,
+      name: 'Johanna',
+      sexe: 'Masculin',
+      job: 'Voleur',
+      race: 'Elfe',
+      level: 4,
+      experience: 0,
+      destiny_point: 0,
+      gold: 250,
+      stats: {
+        bravery: 10,
+        intelligence: 11,
+        charisma: 8,
+        dexterity: 9,
+        strength: 8,
+        health: 25,
+        mana: 0,
+        armor: 0,
+        attack: 10,
+        parry: 10
+      },
+      skills: [
+        {
+          name: 'Test',
+          effect: 'test'
+        },
+        {
+          name: 'Test',
+          effect: 'test'
+        }
+      ],
+      weapons: [],
+      protections: [],
+      description: 'description'
     }
   ]
 ;
